99c95ea3d0dc0a09e1b0b9c69bc83440b7a4b283
Stephen Payette <stephenpayette@Stephens-MacBook-Pro.local>
2021-07-01 18:50:02 -0230

    Connection to API
    Connection established with API and displaying JSON in console log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-let heading = document.getElementById("heading");
-let btn1 = document.getElementById("button");
+var button = document.getElementById("button");
 
-btn1.addEventListener("click", ()=>{
-    console.log("i have been clicked");
-    heading.textContent = "Welcome";
+button.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('get','https://api.themoviedb.org/3/movie/200?api_key=60d70c72ef8913b3400910c0f869e6b6')
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+        console.log(ourData);
+    };
+    ourRequest.send();
 });
 
+// Enter random integer to enter into URL 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+
+function renderHTML(data) {
+
+}
